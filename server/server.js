a12371f2db2b883af8cfb90f8936f70e1c0c9407
Monish Kapadia <monishk10@yahoo.com>
2020-10-02 15:59:09 -0400

    Fixed routing errors

--- server/server.js
+++ server/server.js
@@ -28,6 +28,9 @@ io.on('connection', (socket) => {
 app.use(cors());
 app.use(express.json());
 app.use(express.static(__dirname + '/public'));
+app.get('*', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
 
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, {
